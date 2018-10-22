0e785dc6209409145b8b6c0fc7a127aaeb324889
Lorenzo Tabasso <tabasso.lorenzo@educ.di.unito.it>
2018-10-22 16:23:38 +0200

    working on usage 2

--- ex1/Sorts.c
+++ ex1/Sorts.c
@@ -6,8 +6,12 @@
 
 // path completo "/Volumes/HDD/Lorenzo/Unito/2 Anno/ASD/Progetto/Progetto 2017-2018/laboratorio-algoritmi-2017-18/Datasets/ex1/integers.csv"
 // path relativo "../../laboratorio-algoritmi-2017-18/Datasets/ex1/integers.csv"
-#define DATASET_PATH "../../laboratorio-algoritmi-2017-18/Datasets/ex1/integers.csv"
-#define ELEMENTS_TO_SCAN 20
+#define DATASET_PATH_INTEGERS "../../laboratorio-algoritmi-2017-18/Datasets/ex1/integers.csv"
+#define DATASET_PATH_SUMS "../../laboratorio-algoritmi-2017-18/Datasets/ex1/sums.txt"
+#define ELEMENTS_TO_SCAN 20000000
+#define ELEMENTS_TO_TEST 11
+
+int test = 0;
 
 /*
  * Insertionsort (200000 records) = 1:14 min
@@ -16,11 +20,11 @@
 // Functions prototypes ------------------------------------------------------------------------------------------------
 
 void insertionSort(void ** arrayToSort, int compareInsertSort(void* array, int i, void* value));
-void mergeSort(void ** arrayToSort, int leftIndex, int rightIndex, int compareMergeSort(void * firstValue, void * secondValue));
-void merge(void** arrayToSort, int left, int center, int right, int compare2(void * firstValue, void * secondValue));
-int usoDue(void ** arrayToTest void ** arrayToSort, int left, int center, int right, int compareMergeSort(void * firstValue, void * secondValue));
+void mergeSort(void ** arrayToSort, int leftIndex, int rightIndex, int compareMergeSort(void * firstValue, void * secondValue), void ** arrayToTest);
+void merge(void** arrayToSort, int left, int center, int right, int compare2(void * firstValue, void * secondValue), void ** arrayToTest);
+//int usoDue(void ** arrayToTest void ** arrayToSort, int left, int center, int right, int compareMergeSort(void * firstValue, void * secondValue));
 
-void read(char pathToDataset[], void ** arrayToFill);
+void read(char pathToDataset[], void ** arrayToFill, int arrayLength);
 void printArray(void ** arrayToPrint, int arrayLength);
 
 int compareInsertSort(void* array, int i, void* value);
@@ -44,16 +48,16 @@ void insertionSort(void ** arrayToSort, int compareInsertSort(void* array, int i
     }
 }
 
-void mergeSort(void ** arrayToSort, int leftIndex, int rightIndex, int compareMergeSort(void * firstValue, void * secondValue)){
+void mergeSort(void ** arrayToSort, int leftIndex, int rightIndex, int compareMergeSort(void * firstValue, void * secondValue), void ** arrayToTest){
     if(leftIndex < rightIndex){
         int center = (leftIndex + rightIndex) / 2;
-        mergeSort(arrayToSort, leftIndex, center, compareMergeSort);
-        mergeSort(arrayToSort, center + 1, rightIndex, compareMergeSort);
-        merge(arrayToSort, leftIndex, center, rightIndex, compareMergeSort);
+        mergeSort(arrayToSort, leftIndex, center, compareMergeSort, arrayToTest);
+        mergeSort(arrayToSort, center + 1, rightIndex, compareMergeSort, arrayToTest);
+        merge(arrayToSort, leftIndex, center, rightIndex, compareMergeSort, arrayToTest);
     }
 }
 
-void merge(void ** arrayToSort, int left, int center, int right, int compareMergeSort(void * firstValue, void * secondValue)){
+void merge(void ** arrayToSort, int left, int center, int right, int compareMergeSort(void * firstValue, void * secondValue), void ** arrayToTest){
     int n1 = (center - left + 1);
     int n2 = (right - center);
     int i, j, k;
@@ -82,33 +86,55 @@ void merge(void ** arrayToSort, int left, int center, int right, int compareMerg
             *(arrayToSort+ k) = *(leftSubArray + i);
             i++;
         } else {
-            *(arrayToSort+ k) = *(rightSubArray+ j);
+            *(arrayToSort + k) = *(rightSubArray + j);
             j++;
         }
+        
+        // USAGE 2 ----------------------------------------------------------------------------------------------------
+
+        int sizeLeft = sizeof(leftSubArray)/sizeof(leftSubArray[0]);
+        int sizeRight = sizeof(rightSubArray)/sizeof(rightSubArray[0]);
+
+        int i1 = 0; 
+        int i2 = sizeRight;
+        int z = ELEMENTS_TO_TEST;
+        while(z >= 0 && i1 < sizeLeft && i2 >= 0) {
+            
+            if ((unsigned long long) *(leftSubArray + i1) + (unsigned long long) *(rightSubArray + i2) < (unsigned long long) *(arrayToTest + z)) {
+                i1++;
+            }
+            else if((unsigned long long) *(leftSubArray + i1) + (unsigned long long) *(rightSubArray + i2) > (unsigned long long) *(arrayToTest + z)) {
+                i2--;
+            }
+            else {
+                test = test + 1;
+            }
+        }
+        
     }
 
     free(leftSubArray);
     free(rightSubArray);
 }
 
-int usoDue(void ** arrayToTest, void ** sortedArray, int compareMergeSort(void * firstValue, void * secondValue)) {
-    int j = ELEMENTS_TO_SCAN;
+// int usoDue(void ** arrayToTest, void ** sortedArray, int compareMergeSort(void * firstValue, void * secondValue)) {
+//     int j = ELEMENTS_TO_SCAN;
 
-    for(size_t i = 0; i < ELEMENTS_TO_SCAN && j >=0 ; i++) {
-    //     if ( *(sortedArray+i) + *(sortedArray+j) < x)
-    //     ++i1
-    // else if A1[i1] + A2[i2] > x
-    //     --i2
-    // else
-    //     success!!!!
-    }
+//     for(size_t i = 0; i < ELEMENTS_TO_SCAN && j >=0 ; i++) {
+//     //     if ( *(sortedArray+i) + *(sortedArray+j) < x)
+//     //     ++i1
+//     // else if A1[i1] + A2[i2] > x
+//     //     --i2
+//     // else
+//     //     success!!!!
+//     }
     
     
-}
+// }
 
 // Support functions ---------------------------------------------------------------------------------------------------
 
-void read(char pathToDataset[], void ** arrayToFill) {
+void read(char pathToDataset[], void ** arrayToFill, int arrayLength) {
     clock_t start = clock(); // for timing
 
     FILE* dataset = fopen(pathToDataset, "r");
@@ -120,7 +146,7 @@ void read(char pathToDataset[], void ** arrayToFill) {
     int i = 0;
 
     // TODO: chiedere a Damiani se il cast va bene!!
-    while (i < ELEMENTS_TO_SCAN && fscanf(dataset, "%llu", (unsigned long long *) &arrayToFill[i]) != EOF) {
+    while (i < arrayLength && fscanf(dataset, "%llu", (unsigned long long *) &arrayToFill[i]) != EOF) {
         //printf("line: %d.\n", i);  // ONLY FOR DEBUG, it wil print 20ML of lines!
         i++;
     }
@@ -154,7 +180,11 @@ int main(int argc, char *argv[]) {
     //toSort = (void **) malloc(ELEMENTS_TO_SCAN * sizeof(unsigned long long)); // funge
     toSort = (void **) malloc(ELEMENTS_TO_SCAN * sizeof(void *)); // funge
 
-    read(DATASET_PATH, toSort);
+    void * toTest;
+    toTest = (void **) malloc(ELEMENTS_TO_TEST * sizeof(void *)); // funge
+
+    read(DATASET_PATH_INTEGERS, toSort, ELEMENTS_TO_SCAN);
+    read(DATASET_PATH_SUMS, toTest, ELEMENTS_TO_TEST);
 
     if (strcmp(argv[1], "-print") == 0) {
         printArray(toSort, ELEMENTS_TO_SCAN);
@@ -164,8 +194,9 @@ int main(int argc, char *argv[]) {
             printf("InsertionSort finished\n");
         }
         else {
-            mergeSort(toSort,0, ELEMENTS_TO_SCAN-1, compareMergeSort);
+            mergeSort(toSort,0, ELEMENTS_TO_SCAN-1, compareMergeSort, toTest);
             printf("Merge finished\n");
+            printf("TEST: %d\n", test);
         }
 
         printArray(toSort, ELEMENTS_TO_SCAN);
@@ -176,8 +207,9 @@ int main(int argc, char *argv[]) {
             printf("InsertionSort finished\n");
         }
         else {
-            mergeSort(toSort,0, ELEMENTS_TO_SCAN-1, compareMergeSort);
+            mergeSort(toSort,0, ELEMENTS_TO_SCAN-1, compareMergeSort, toTest);
             printf("Merge finished\n");
+            printf("TEST: %d\n", test);
         }
     }
 
