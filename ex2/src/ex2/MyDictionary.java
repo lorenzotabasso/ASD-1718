31ff5f78d21363668583d37236699c5de23265a5
Lorenzo Tabasso <tabasso.lorenzo@educ.di.unito.it>
2018-11-19 23:42:53 +0100

    load method working

--- ex2/src/ex2/MyDictionary.java
+++ ex2/src/ex2/MyDictionary.java
@@ -1,7 +1,9 @@
 package ex2;
 
-import java.util.*;
-import java.io.*;
+
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * @author Lorenzo Tabasso
@@ -10,77 +12,108 @@ import java.io.*;
 
 class MyDictionary {
 
-    public HashMap<Character, HashMap<Integer, String>> index = new HashMap<Character, HashMap<Integer, String>>();
-    public Integer sectionsNumber = 0;
-    public Integer wordsNumber = 0;
+    private HashMap<Character, HashMap<Integer, String>> index;
+    private Integer sectionsNumber;
+    private Integer wordsNumber;
+
+    // COSTRUCTORS ------------------------------------------------------------
+
+    public MyDictionary() {
+        this.index = new HashMap<Character, HashMap<Integer, String>>();
+        this.sectionsNumber = 0;
+        this.wordsNumber = 0;
+    }
+
+    // GETTERS-----------------------------------------------------------------
+
+    public HashMap<Character, HashMap<Integer, String>> getIndex() {
+        return index;
+    }
+
+    public Integer getSectionsNumber() {
+        return sectionsNumber;
+    }
+
+    public Integer getWordsNumber() {
+        return wordsNumber;
+    }
+
+    // SETTERS ----------------------------------------------------------------
+
+    public void setIndex(HashMap<Character, HashMap<Integer, String>> index) {
+        this.index = index;
+    }
+
+    public void setSectionsNumber(Integer sectionsNumber) {
+        this.sectionsNumber = sectionsNumber;
+    }
+
+    public void setWordsNumber(Integer wordsNumber) {
+        this.wordsNumber = wordsNumber;
+    }
 
     public void addSection(Character section) {
-        if (!index.containsKey(section)) {
-            index.put(section, new HashMap<Integer, String>());
+        if (!getIndex().containsKey(section)) {
+            getIndex().put(section, new HashMap<Integer, String>());
             sectionsNumber++;
         }
     }
 
-    public HashMap getSection(Character letter) {
-        return index.get(letter);
+    public HashMap<Integer, String> getSection(Character letter) {
+        return getIndex().get(letter);
     }
 
     public void addWord(String word) {
         Character c = word.charAt(0);
-        if (!index.containsKey(c)) {
-            index.put(c, new HashMap<Integer, String>());
+        if (!getIndex().containsKey(c)) {
+            getIndex().put(c, new HashMap<Integer, String>());
             sectionsNumber++;
-        } else {
-            HashMap section = index.get(c);
-            if (!section.containsKey(word)) {
-                section.put(wordsNumber, word);
-                wordsNumber++;
-            }
         }
-
+        HashMap<Integer, String> section = getIndex().get(c);
+        if (!section.containsValue(word)) {
+            section.put(wordsNumber, word);
+            wordsNumber++;
+        }
     }
 
     public void printSections(){
-        for (Character var : index.keySet()) {
-            char key = var;
+        for (Character key : getIndex().keySet()) {
             System.out.println("Section: " + key);
         }
     }
 
     /**
-     * This method reads records, from a csv file, with this structure:
-     * 'integer field' 'character' 'string field' 'character' 'integer field' 'character' 'floating point field' \n.
+     * Reads and load one word at time from the dataset
+     * (wich path had to be specified in datasetPath parameter) into
+     * MyDictionary object.
+     *
      * @param datasetPath: the path to the dataset to load.
      */
 
     public void load (String datasetPath) {
         try {
-            if (datasetPath == null) {
+            if (datasetPath == null)
                 throw new RuntimeException("Error while opening the file, retry.");
-            }
+
             System.out.println("Loading dataset into Dictionary ...");
             long start = System.currentTimeMillis();
 
             MyDictionary dict = new MyDictionary();
+            Scanner sc = new Scanner(new FileInputStream(datasetPath));
+            sc.useDelimiter("\n");
 
-            BufferedReader br = new BufferedReader(new FileReader(datasetPath));
-            while (br.readLine() != null) {
-                dict.addWord(br.readLine());
-            }
-            br.close();
-
-            // 2. For-each Loop
-            for (Character key : index.keySet()) {
-                System.out.println(key);
-            }
+            while (sc.hasNext())
+                dict.addWord(sc.nextLine());
+            sc.close();
 
             long end = System.currentTimeMillis();
-            System.out.println("Loaded " + dict.sectionsNumber + " Sections and " + dict.wordsNumber + " Words in " + (float)(end - start)/1000 + " seconds.");
-        }
-
-        catch (Exception e) {
+            System.out.println("Loaded " + dict.sectionsNumber + " Sections and "
+                    + dict.wordsNumber + " Words in "
+                    + (float)(end - start)/1000 + " seconds.");
+        } catch (Exception e) {
             throw new RuntimeException("Error while reading dataset.");
-        }
-    }
+        } // end try-catch
 
-}
+    } // end load method
+
+} // end MyDictionary class
