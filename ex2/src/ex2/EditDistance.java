faeffa86e362d268a56e446d13ef1346b4e1c34f
Lorenzo Tabasso <tabasso.lorenzo@educ.di.unito.it>
2018-11-22 23:59:59 +0100

    working on editDistance method - 2

--- ex2/src/ex2/EditDistance.java
+++ ex2/src/ex2/EditDistance.java
@@ -20,10 +20,9 @@ public class EditDistance {
 //        else return z;
 //    }
 
-    static int min(int x,int y /*,int z*/) {
-        if (x <= y /*&& x <= z*/) return x;
-        else /*if (y <= x && y <= z) */ return y;
-        //else return z;
+    static int min(int x,int y) {
+        if (x <= y) return x;
+        else return y;
     }
 
     static int editDistance(String str1 , String str2 , int m ,int n) {
@@ -41,7 +40,6 @@ public class EditDistance {
         // If last characters of two strings are same, nothing
         // much to do. Ignore last characters and get count for
         // remaining strings.
-
         if (str1.charAt(0) == str2.charAt(0))
             return editDistance(str1.substring(m-(m-1)), str2.substring(n-(n-1)), m-1, n-1);
 
@@ -50,49 +48,50 @@ public class EditDistance {
         // compute minimum cost for all three operations and take
         // minimum of three values.
 
-        int dcanc = 1 + editDistance(str1, str2.substring(n-(n-1)), m, n-1); // Insert
-        int dins = 1 + editDistance(str1.substring(m-(m-1)), str2, m-1, n);   // Remove
+        int dcanc = 1 + editDistance(str1, str2.substring(n-(n-1)), m, n-1); // Remove
+        int dins = 1 + editDistance(str1.substring(m-(m-1)), str2, m-1, n);   // Insert
 
+        // only for debug
         System.out.println("CANC: " + dcanc);
         System.out.println("INS: " + dins);
 
-        return min (dcanc, dins);
+        return min(dcanc, dins);
     }
 
-    static int editDistanceDyn(String str1, String str2, int m, int n) {
-
-        // Create a table to store results of subproblems
-        int dp[][] = new int[m+1][n+1];
-
-        // Fill d[][] in bottom up manner
-        for (int i=0; i<=m; i++) {
-            for (int j=0; j<=n; j++) {
-                // If first string is empty, only option is to
-                // isnert all characters of second string
-                if (i==0)
-                    dp[i][j] = j; // Min. operations = j
-
-                    // If second string is empty, only option is to
-                    // remove all characters of second string
-                else if (j==0)
-                    dp[i][j] = i; // Min. operations = i
-
-                    // If last characters are same, ignore last char
-                    // and recur for remaining string
-                else if (str1.charAt(i-1) == str2.charAt(j-1))
-                    dp[i][j] = dp[i-1][j-1];
-
-                    // If the last character is different, consider all
-                    // possibilities and find the minimum
-                else
-                    dp[i][j] = 1 + min(dp[i][j-1], // Insert
-                            dp[i-1][j] /*, // Remove
-									dp[i-1][j-1]*/); // Replace
-            }
-        }
-
-        return dp[m][n];
-    }
+//    static int editDistanceDyn(String str1, String str2, int m, int n) {
+//
+//        // Create a table to store results of subproblems
+//        int dp[][] = new int[m+1][n+1];
+//
+//        // Fill d[][] in bottom up manner
+//        for (int i=0; i<=m; i++) {
+//            for (int j=0; j<=n; j++) {
+//                // If first string is empty, only option is to
+//                // isnert all characters of second string
+//                if (i==0)
+//                    dp[i][j] = j; // Min. operations = j
+//
+//                    // If second string is empty, only option is to
+//                    // remove all characters of second string
+//                else if (j==0)
+//                    dp[i][j] = i; // Min. operations = i
+//
+//                    // If last characters are same, ignore last char
+//                    // and recur for remaining string
+//                else if (str1.charAt(i-1) == str2.charAt(j-1))
+//                    dp[i][j] = dp[i-1][j-1];
+//
+//                    // If the last character is different, consider all
+//                    // possibilities and find the minimum
+//                else
+//                    dp[i][j] = 1 + min(dp[i][j-1], // Insert
+//                            dp[i-1][j] /*, // Remove
+//									dp[i-1][j-1]*/); // Replace
+//            }
+//        }
+//
+//        return dp[m][n];
+//    }
 
     public static void main(String args[]) {
 
