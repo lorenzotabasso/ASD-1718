88841422539795b2a6a31333d49159f369a8b3b5
Lorenzo Tabasso <tabasso.lorenzo@educ.di.unito.it>
2018-11-22 23:52:12 +0100

    working on editDistance method

--- ex2/src/ex2/EditDistance.java
+++ ex2/src/ex2/EditDistance.java
@@ -14,6 +14,12 @@ public class EditDistance {
 
     String fileToCorrect[];
 
+//    static int min(int x,int y ,int z) {
+//        if (x <= y && x <= z) return x;
+//        else if (y <= x && y <= z) return y;
+//        else return z;
+//    }
+
     static int min(int x,int y /*,int z*/) {
         if (x <= y /*&& x <= z*/) return x;
         else /*if (y <= x && y <= z) */ return y;
@@ -22,30 +28,35 @@ public class EditDistance {
 
     static int editDistance(String str1 , String str2 , int m ,int n) {
 
-        //System.out.println("Computing the edit distance between: " + str1 + ", " + str2);
-
         // If first string is empty, the only option is to
         // insert all characters of second string into first
+        // if |s1| = 0 then editDistance(s1,s2) = |s2|
         if (m == 0) return n;
 
         // If second string is empty, the only option is to
         // remove all characters of first string
+        // if |s2| = 0 then editDistance(s1,s2) = |s1|
         if (n == 0) return m;
 
         // If last characters of two strings are same, nothing
         // much to do. Ignore last characters and get count for
         // remaining strings.
-        if (str1.charAt(m-1) == str2.charAt(n-1))
-            return editDistance(str1, str2, m-1, n-1);
+
+        if (str1.charAt(0) == str2.charAt(0))
+            return editDistance(str1.substring(m-(m-1)), str2.substring(n-(n-1)), m-1, n-1);
 
         // If last characters are not same, consider all three
         // operations on last character of first string, recursively
         // compute minimum cost for all three operations and take
         // minimum of three values.
-        return 1 + min ( editDistance(str1, str2, m, n-1),    // Insert
-                editDistance(str1, str2, m-1, n) /*,   // Remove
-						editDistance(str1, str2, m-1, n-1) // Replace */
-        );
+
+        int dcanc = 1 + editDistance(str1, str2.substring(n-(n-1)), m, n-1); // Insert
+        int dins = 1 + editDistance(str1.substring(m-(m-1)), str2, m-1, n);   // Remove
+
+        System.out.println("CANC: " + dcanc);
+        System.out.println("INS: " + dins);
+
+        return min (dcanc, dins);
     }
 
     static int editDistanceDyn(String str1, String str2, int m, int n) {
@@ -85,15 +96,15 @@ public class EditDistance {
 
     public static void main(String args[]) {
 
-        MyDictionary dict = new MyDictionary();
-        dict.load("/Volumes/HDD/Lorenzo/Unito/2 Anno/ASD/Progetto/Progetto 2017-2018/laboratorio-algoritmi-2017-18/Datasets/ex2/dictionary.txt");
+        //MyDictionary dict = new MyDictionary();
+        //dict.load("/Volumes/HDD/Lorenzo/Unito/2 Anno/ASD/Progetto/Progetto 2017-2018/laboratorio-algoritmi-2017-18/Datasets/ex2/dictionary.txt");
 
         String str1 = "casa";
         String str2 = "cassa";
 
         System.out.println("Computing the edit distance between: " + str1 + ", " + str2);
         System.out.println(editDistance( str1 , str2 , str1.length(), str2.length()));
-        System.out.println(editDistanceDyn( str1 , str2 , str1.length(), str2.length()));
+        //System.out.println(editDistanceDyn( str1 , str2 , str1.length(), str2.length()));
 
         String str3 = "casa";
         String str4 = "cara";
